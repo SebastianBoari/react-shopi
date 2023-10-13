5b70ff40399a536b2a6ac8fac71e6e0950a0f3a1
sebastian <luciosebastianboari@gmail.com>
2023-10-12 23:39:18 -0300

    feat: filter duplicate products on cart

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
@@ -30,6 +30,29 @@ const Card = (data) => {
     openCheckoutSideMenu()
   }
 
+  const renderIcon = (id) => {
+    const isInCart = cartProducts.filter(product => product.id === id).length > 0
+
+    if(isInCart){
+      return (
+        <div 
+        className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'>
+            <CheckIcon className='h-6 w-6 text-white'>
+            </CheckIcon>
+        </div>
+      )
+    } else {
+      return (
+        <div 
+        className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+        onClick={(event) => addProductsToCart(event, data.data)}>
+            <PlusIcon className='h-6 w-6 text-black'>
+            </PlusIcon>
+        </div>
+      )
+    }
+  }
+
   return (
     <div
     className='bg-white cursor-pointer w-56 h-60 rounded-lg'
@@ -40,13 +63,7 @@ const Card = (data) => {
             
             <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt='headphones'/>
             
-            <div 
-            className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-            onClick={(event) => addProductsToCart(event, data.data)}>
-                <PlusIcon className='h-6 w-6 text-black'>
-                </PlusIcon>
-            </div>
-
+            {renderIcon(data.data.id)}
         </figure>
 
         <p className='flex justify-between items-center'>
--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -20,7 +20,7 @@ const CheckoutSideMenu = () => {
             </div>
         </div>
 
-        <div className='px-6'>
+        <div className='px-6 overflow-y-scroll'>
         {
           cartProducts?.map((product) => (
             <OrderCard
